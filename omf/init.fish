71212f496a2f2a998de4979e8861b8611880ca0a
Noah Moroze <noahmoroze@gmail.com>
2020-01-17 17:34:06 -0500

    Make VS Code linting and formatting work nicely

--- omf/init.fish
+++ omf/init.fish
@@ -4,8 +4,8 @@ set -g -x PATH /usr/local/bin $PATH
 # Add Homebrew /sbin to path
 set -g -x PATH /usr/local/sbin $PATH
 
-# Add Haskell Stack install directory
-set -g -x PATH /Users/noah/.local/bin/ $PATH
+# Add Haskell Stack/pipx install directory
+set -g -x PATH /Users/noah/.local/bin $PATH
 
 # Disable virtualenv prompt change since my custom prompt already has it
 set -g -x VIRTUAL_ENV_DISABLE_PROMPT 1
