a91b47ce79dc72a221c86479455e0ecbc30fed0c
Noah Moroze <noahmoroze@gmail.com>
2021-01-18 16:52:28 -0500

    Track iTerm2 settings

---
+++ iterm/setup-iterm.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+## Courstesy of http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
